ad4882c0dcf9c5250f26f83e166e10012f75970c
github username <github email address>
2014-07-06 09:24:12 -0400

    Reorganized storyboard, included new View for Demo.

---
+++ BetaBuildBB/BBDemoMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  BBDemoMainViewController.h
+//  BetaBuildBB
+//
+//  Created by Troy Barrett on 7/6/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BBDemoMainViewController : UIViewController
+
+@end
